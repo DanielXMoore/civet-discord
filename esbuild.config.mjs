a97a540a81c1363b84c47c83bfee9d9ca6352c17
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2023-01-09 06:16:23 +0100

    Init

---
+++ esbuild.config.mjs
@@ -0,0 +1,14 @@
+import esbuild from "esbuild";
+import civetPlugin from "@danielx/civet/esbuild-plugin";
+
+esbuild
+  .build({
+    entryPoints: ["src/index.civet"],
+    bundle: true,
+    platform: "node",
+    outdir: "dist",
+    target: "node18",
+    plugins: [civetPlugin],
+    external: ["dotenv", "@danielx/civet", "discord.js", "prettier"],
+  })
+  .catch(() => process.exit(1));
